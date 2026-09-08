eccdf84ccc177c05c03454ae3e4b56b02034d104
amandeep <amandeep@gmail.com>
2026-09-08 16:31:29 +0530

    completed the middlewaRE

---
+++ backend/internal/middleware/cors.go
@@ -0,0 +1,21 @@
+package middleware
+
+import (
+	"strings"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func CORS() gin.HandlerFunc {
+	return cors.New(cors.Config{
+		AllowOriginFunc: func(origin string) bool {
+			return strings.HasPrefix(origin, "http://localhost:")
+		},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	})
+}
